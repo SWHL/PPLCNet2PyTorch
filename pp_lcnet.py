9db7e3b88fe33decd3584e462be5672becb608ae
SWHL <liekkaskono@163.com>
2021-11-10 20:14:21 +0800

    Update files.

--- pp_lcnet.py
+++ pp_lcnet.py
@@ -11,18 +11,22 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 from __future__ import absolute_import, division, print_function
 
+import os
+from functools import partial
+
+import cv2
+import numpy as np
 import paddle
 import paddle.nn as nn
+import paddle.nn.functional as F
+import six
 from paddle import ParamAttr
 from paddle.nn import AdaptiveAvgPool2D, BatchNorm, Conv2D, Dropout, Linear
 from paddle.nn.initializer import KaimingNormal
 from paddle.regularizer import L2Decay
-
-from save_load import load_dygraph_pretrain, load_dygraph_pretrain_from_url
-from theseus_layer import TheseusLayer
+from PIL import Image
 
 MODEL_URLS = {
     "PPLCNet_x0_25":
@@ -64,6 +68,15 @@ NET_CONFIG = {
 }
 
 
+def load_dygraph_pretrain(model, path=None):
+    if not (os.path.isdir(path) or os.path.exists(path + '.pdparams')):
+        raise ValueError("Model pretrain path {} does not "
+                         "exists.".format(path))
+    param_state_dict = paddle.load(path + ".pdparams")
+    model.set_dict(param_state_dict)
+    return
+
+
 def make_divisible(v, divisor=8, min_value=None):
     if min_value is None:
         min_value = divisor
@@ -74,7 +87,7 @@ def make_divisible(v, divisor=8, min_value=None):
     return new_v
 
 
-class ConvBNLayer(TheseusLayer):
+class ConvBNLayer(nn.Layer):
     def __init__(self,
                  num_channels,
                  filter_size,
@@ -100,13 +113,15 @@ class ConvBNLayer(TheseusLayer):
         self.hardswish = nn.Hardswish()
 
     def forward(self, x):
+        # x.shape: 1x16x112x112
         x = self.conv(x)
+        # x.shape 1x32x112x112
         x = self.bn(x)
         x = self.hardswish(x)
         return x
 
 
-class DepthwiseSeparable(TheseusLayer):
+class DepthwiseSeparable(nn.Layer):
     def __init__(self,
                  num_channels,
                  num_filters,
@@ -131,13 +146,15 @@ class DepthwiseSeparable(TheseusLayer):
 
     def forward(self, x):
         x = self.dw_conv(x)
+
         if self.use_se:
             x = self.se(x)
+
         x = self.pw_conv(x)
         return x
 
 
-class SEModule(TheseusLayer):
+class SEModule(nn.Layer):
     def __init__(self, channel, reduction=4):
         super().__init__()
         self.avg_pool = AdaptiveAvgPool2D(1)
@@ -167,7 +184,7 @@ class SEModule(TheseusLayer):
         return x
 
 
-class PPLCNet(TheseusLayer):
+class PPLCNet(nn.Layer):
     def __init__(self,
                  scale=1.0,
                  class_num=1000,
@@ -267,12 +284,8 @@ class PPLCNet(TheseusLayer):
         return x
 
 
-def _load_pretrained(pretrained, model, model_url, use_ssld):
-    if pretrained is False:
-        pass
-    elif pretrained is True:
-        load_dygraph_pretrain_from_url(model, model_url, use_ssld=use_ssld)
-    elif isinstance(pretrained, str):
+def _load_pretrained(pretrained, model, *args):
+    if isinstance(pretrained, str):
         load_dygraph_pretrain(model, pretrained)
     else:
         raise RuntimeError(
@@ -400,13 +413,237 @@ def PPLCNet_x2_5(pretrained=False, use_ssld=False, **kwargs):
     return model
 
 
+class UnifiedResize(object):
+    def __init__(self, interpolation=None, backend="cv2"):
+        _cv2_interp_from_str = {
+            'nearest': cv2.INTER_NEAREST,
+            'bilinear': cv2.INTER_LINEAR,
+            'area': cv2.INTER_AREA,
+            'bicubic': cv2.INTER_CUBIC,
+            'lanczos': cv2.INTER_LANCZOS4
+        }
+        _pil_interp_from_str = {
+            'nearest': Image.NEAREST,
+            'bilinear': Image.BILINEAR,
+            'bicubic': Image.BICUBIC,
+            'box': Image.BOX,
+            'lanczos': Image.LANCZOS,
+            'hamming': Image.HAMMING
+        }
+
+        def _pil_resize(src, size, resample):
+            pil_img = Image.fromarray(src)
+            pil_img = pil_img.resize(size, resample)
+            return np.asarray(pil_img)
+
+        if backend.lower() == "cv2":
+            if isinstance(interpolation, str):
+                interpolation = _cv2_interp_from_str[interpolation.lower()]
+            # compatible with opencv < version 4.4.0
+            elif interpolation is None:
+                interpolation = cv2.INTER_LINEAR
+            self.resize_func = partial(cv2.resize, interpolation=interpolation)
+        elif backend.lower() == "pil":
+            if isinstance(interpolation, str):
+                interpolation = _pil_interp_from_str[interpolation.lower()]
+            self.resize_func = partial(_pil_resize, resample=interpolation)
+        else:
+            print(
+                f"The backend of Resize only support \"cv2\" or \"PIL\". \"f{backend}\" is unavailable. Use \"cv2\" instead."
+            )
+            self.resize_func = cv2.resize
+
+    def __call__(self, src, size):
+        return self.resize_func(src, size)
+
+
+class DecodeImage(object):
+    """ decode image """
+
+    def __init__(self, to_rgb=True, to_np=False, channel_first=False):
+        self.to_rgb = to_rgb
+        self.to_np = to_np  # to numpy
+        self.channel_first = channel_first  # only enabled when to_np is True
+
+    def __call__(self, img):
+        if six.PY2:
+            assert type(img) is str and len(
+                img) > 0, "invalid input 'img' in DecodeImage"
+        else:
+            assert type(img) is bytes and len(
+                img) > 0, "invalid input 'img' in DecodeImage"
+        data = np.frombuffer(img, dtype='uint8')
+        img = cv2.imdecode(data, 1)
+        if self.to_rgb:
+            assert img.shape[2] == 3, 'invalid shape of image[%s]' % (
+                img.shape)
+            img = img[:, :, ::-1]
+
+        if self.channel_first:
+            img = img.transpose((2, 0, 1))
+
+        return img
+
+
+class ResizeImage(object):
+    """ resize image """
+
+    def __init__(self,
+                 size=None,
+                 resize_short=None,
+                 interpolation=None,
+                 backend="cv2"):
+        if resize_short is not None and resize_short > 0:
+            self.resize_short = resize_short
+            self.w = None
+            self.h = None
+        elif size is not None:
+            self.resize_short = None
+            self.w = size if type(size) is int else size[0]
+            self.h = size if type(size) is int else size[1]
+        else:
+            raise ValueError("invalid params for ReisizeImage for '\
+                'both 'size' and 'resize_short' are None")
+
+        self._resize_func = UnifiedResize(
+            interpolation=interpolation, backend=backend)
+
+    def __call__(self, img):
+        img_h, img_w = img.shape[:2]
+        if self.resize_short is not None:
+            percent = float(self.resize_short) / min(img_w, img_h)
+            w = int(round(img_w * percent))
+            h = int(round(img_h * percent))
+        else:
+            w = self.w
+            h = self.h
+        return self._resize_func(img, (w, h))
+
+
+class CropImage(object):
+    """ crop image """
+
+    def __init__(self, size):
+        if type(size) is int:
+            self.size = (size, size)
+        else:
+            self.size = size  # (h, w)
+
+    def __call__(self, img):
+        w, h = self.size
+        img_h, img_w = img.shape[:2]
+        w_start = (img_w - w) // 2
+        h_start = (img_h - h) // 2
+
+        w_end = w_start + w
+        h_end = h_start + h
+        return img[h_start:h_end, w_start:w_end, :]
+
+
+class NormalizeImage(object):
+    """ normalize image such as substract mean, divide std
+    """
+
+    def __init__(self,
+                 scale=None,
+                 mean=None,
+                 std=None,
+                 order='chw',
+                 output_fp16=False,
+                 channel_num=3):
+        if isinstance(scale, str):
+            scale = eval(scale)
+        assert channel_num in [
+            3, 4
+        ], "channel number of input image should be set to 3 or 4."
+        self.channel_num = channel_num
+        self.output_dtype = 'float16' if output_fp16 else 'float32'
+        self.scale = np.float32(scale if scale is not None else 1.0 / 255.0)
+        self.order = order
+        mean = mean if mean is not None else [0.485, 0.456, 0.406]
+        std = std if std is not None else [0.229, 0.224, 0.225]
+
+        shape = (3, 1, 1) if self.order == 'chw' else (1, 1, 3)
+        self.mean = np.array(mean).reshape(shape).astype('float32')
+        self.std = np.array(std).reshape(shape).astype('float32')
+
+    def __call__(self, img):
+        from PIL import Image
+        if isinstance(img, Image.Image):
+            img = np.array(img)
+
+        assert isinstance(img,
+                          np.ndarray), "invalid input 'img' in NormalizeImage"
+
+        img = (img.astype('float32') * self.scale - self.mean) / self.std
+
+        if self.channel_num == 4:
+            img_h = img.shape[1] if self.order == 'chw' else img.shape[0]
+            img_w = img.shape[2] if self.order == 'chw' else img.shape[1]
+            pad_zeros = np.zeros(
+                (1, img_h, img_w)) if self.order == 'chw' else np.zeros(
+                    (img_h, img_w, 1))
+            img = (np.concatenate(
+                (img, pad_zeros), axis=0)
+                   if self.order == 'chw' else np.concatenate(
+                       (img, pad_zeros), axis=2))
+        return img.astype(self.output_dtype)
+
+
+class ToCHWImage(object):
+    """ convert hwc image to chw image
+    """
+
+    def __init__(self):
+        pass
+
+    def __call__(self, img):
+        from PIL import Image
+        if isinstance(img, Image.Image):
+            img = np.array(img)
+
+        return img.transpose((2, 0, 1))
+
+
 if __name__ == '__main__':
-    paddle.enable_static()
-    x = paddle.fluid.data(name='pixel',
-                          shape=[None, 3, 224, 224],
-                          dtype='float32')
+    with open('images/imagenet1k_label_list.txt', 'r', encoding='utf-8') as f:
+        label_list = f.readlines()
 
-    model = PPLCNet_x1_0()
+    decode_img = DecodeImage(to_rgb=True, channel_first=False)
+    resize_img = ResizeImage(resize_short=256)
+    crop_img = CropImage(size=224)
 
-    y = model(x)
-    print(y.shape)
+    scale = 1 / 255.0
+    mean = [0.485, 0.456, 0.406]
+    std = [0.229, 0.224, 0.225]
+
+    normalize_img = NormalizeImage(scale, mean, std, order='')
+
+    to_chw_img = ToCHWImage()
+
+    img_path = 'images/demo.jpg'
+    with open(img_path, 'rb') as f:
+        x = f.read()
+
+    x = decode_img(x)
+    x = resize_img(x)
+    x = crop_img(x)
+    x = normalize_img(x)
+    x = to_chw_img(x)
+
+    batch_data = []
+    batch_data.append(x)
+    batch_tensor = paddle.to_tensor(batch_data)
+
+    model_path = 'pretrained_models/PPLCNet_x1_0_pretrained'
+    model = PPLCNet_x1_0(pretrained=model_path)
+    model.eval()
+
+    y = model(batch_tensor)
+    y = F.softmax(y, axis=-1)
+
+    y = y.numpy()
+    probs = y[0]
+    index = probs.argsort(axis=0)[-1:][::-1][0]
+    score = probs[index]
+    print(f'{label_list[index].strip()}: {score}')
